6e2605f542df0b5233935d7b9cbe4c5e093a97bb
belidiberlin <81102072+belidiberlin@users.noreply.github.com>
2021-04-08 12:52:03 +0200

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,3 +1,3 @@
 const el = document.querySelector("#snipcart")
 
-console.log(el.setAttribute("data-api-key","OGEzMGRmNmEtYjJjZS00NDk3LWEwMmUtZjc0MzJkYTUyOTQ5NjM3NTIxMzU5NjA1MTkxNDAw"))
+console.log(el.setAttribute("data-api-key","MzNkNDA4NTUtMDU2NC00ZGIyLWJiMTEtZWE5YzAzNTUzMzNkNjM3NTIxMzU5NjA1MTkxNDAw"))
